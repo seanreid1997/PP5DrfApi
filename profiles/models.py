195389909788e0330f6cf09ed87bf451de054f40
seanreid1997 <reid.sean14@yahoo.com>
2022-11-20 16:18:23 +0000

    Create Post Model

--- profiles/models.py
+++ profiles/models.py
@@ -30,3 +30,24 @@ def create_profile(sender, instance, created, **kwargs):
 
 
 post_save.connect(create_profile, sender=User)
+
+
+class Post(models.Model):
+    """
+    Post model, related to 'owner', i.e. a User instance.
+    Default image set so that we can always reference image.url.
+    """
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    image = models.ImageField(
+        upload_to='images/', default='../default_post_rgq6aq', blank=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
