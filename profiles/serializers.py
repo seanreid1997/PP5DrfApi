b972273b5c2857b4174436ab09d26a1a4fe144cc
seanreid1997 <reid.sean14@yahoo.com>
2022-11-27 15:03:35 +0000

    Commit changes for serializers.py

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -13,7 +13,6 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'content',
-            'image', 'is_owner'
+            'id', 'owner', 'created_at', 'updated_at', 'name',
+            'content', 'image', 'is_owner'
         ]
-
