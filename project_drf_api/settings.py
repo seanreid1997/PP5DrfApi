0cc5712ff86595e305661a0ac383b58236108a86
seanreid1997 <reid.sean14@yahoo.com>
2023-01-02 03:33:34 +0000

    Fix bugs

--- project_drf_api/settings.py
+++ project_drf_api/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-   os.environ.get('ALLOWED_HOST'),
+   'https://pp5-drf-api.herokuapp.com/',
    'localhost',
 ]
 
