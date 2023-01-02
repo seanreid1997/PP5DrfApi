2b83d2638a05f02322e3319c8a7549d5bd3ef612
seanreid1997 <reid.sean14@yahoo.com>
2023-01-02 03:39:52 +0000

    Attempt to fix 400 error

--- project_drf_api/settings.py
+++ project_drf_api/settings.py
@@ -63,10 +63,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
-   'https://pp5-drf-api.herokuapp.com/',
-   'localhost',
-]
+ALLOWED_HOSTS = ['https://pp5-drf-api.herokuapp.com', 'localhost']
 
 
 # Application definition
